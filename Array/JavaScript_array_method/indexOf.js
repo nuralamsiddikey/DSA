4b40f2382c03ff9ca1597043193df72b59b7da66
nur <nurcse253@gmail.com>
2024-09-03 16:40:13 +0600

    indexOf method

---
+++ Array/JavaScript_array_method/indexOf.js
@@ -0,0 +1,7 @@
+// indexOf method returns the index number of specified value.
+// if value not exist then return -1
+// second param indicate the index number from where search start. by default it start from 0 index
+
+const fruits = ['Banana','Orange','Apple','Mango']
+const index = fruits.indexOf('Orange',1)
+console.log(index)
