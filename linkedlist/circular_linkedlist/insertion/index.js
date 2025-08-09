33205d263e51fa8626a7bc60af57f2dfcb32e70b
nuralamsiddikey <nur.alam@lyxa.ai>
2025-08-10 02:12:46 +0600

    insert at first of circular linkedlist

---
+++ linkedlist/circular_linkedlist/insertion/index.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+function insertAtFirst(last, data) {
+  const newNode = new Node(data);
+
+  newNode.next = last.next;
+  last.next = newNode;
+  return newNode;
+}
+
+function printList(last) {
+  let curr = last;
+  while (true) {
+    console.log(curr.data);
+    curr = curr.next;
+    if (curr === last) {
+      break;
+    }
+  }
+}
+
+let first = new Node(10);
+let second = new Node(20);
+let third = new Node(30);
+
+first.next = second;
+second.next = third;
+third.next = first;
+
+third = insertAtFirst(third, 100);
+printList(third);
