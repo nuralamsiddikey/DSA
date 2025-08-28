63f22b99aa12445344e033ea13acda87009d29ac
nuralamsiddikey <nur.alam@lyxa.ai>
2025-08-29 00:28:23 +0600

    reduced complexity of max sum

--- sliding_window/max_sum.js
+++ sliding_window/max_sum.js
@@ -1,3 +1,5 @@
+
+// time complexity: O(n2*k)
 function maxSum(arr, size) {
   let len = arr.length - (size - 1);
   let maxSum = 0;
@@ -10,9 +12,29 @@ function maxSum(arr, size) {
       if (subArrSum > maxSum) maxSum = subArrSum;
     }
   }
-  console.log(maxSum);
+  return maxSum
 }
 
-const arr = [1, 2, 3, 4, 5, 6];
-let size = 3
-maxSum(arr, size);
+
+
+// time complexity: O(n*k)
+function maxSum(arr, k, n) {
+  let max_sum = 0;
+  for (let i = 0; i < n - k + 1; i++) {
+    let current_sum = 0;
+    for (let j = 0; j < k; j++) {
+      current_sum += arr[i + j];
+    }
+
+    if (current_sum > max_sum) {
+      max_sum = current_sum;
+      current_sum = 0;
+    }
+  }
+  return max_sum;
+}
+
+const arr = [1,2,3,4,5];
+const k = 3;
+const n = arr.length;
+console.log(maxSum(arr, k, n));
