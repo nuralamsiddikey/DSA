87412d8a8e0404cac3a8b24f510631983aabd3fa
nuralamsiddikey <nur.alam@lyxa.ai>
2025-11-19 01:07:47 +0600

    added leetcode 167-two sum 11

---
+++ Leetcode/two-pointer/167. Two Sum II - Input Array Is Sorted/main.py
@@ -0,0 +1,15 @@
+def twoSum(numbers,target):
+    left = 0
+    right = len(numbers)-1
+    
+    while left < right:
+        if numbers[left]+numbers[right] == target:
+            return [left+1,right+1]
+        elif numbers[left]+numbers[right] < target:
+            left += 1
+        else: 
+            right -= 1
+        
+        
+numbers = [-1,0]
+target = -1
