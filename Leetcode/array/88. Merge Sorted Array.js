e0cf8b63819f5ab4f4e9a750f991c25b0604e1ec
nuralamsiddikey <nur.alam@lyxa.ai>
2025-12-09 23:19:07 +0600

    merge sorted array

---
+++ Leetcode/array/88. Merge Sorted Array.js
@@ -0,0 +1,14 @@
+var merge = function (nums1, m, nums2, n) {
+  for (let i = 0; i < n; i++) {
+    nums1[m + i] = nums2[i];
+  }
+
+  return nums1.sort((a, b) => a - b);
+};
+
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
+
+console.log(merge(nums1, m, nums2, n));
