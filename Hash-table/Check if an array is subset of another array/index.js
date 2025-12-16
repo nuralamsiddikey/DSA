db2aebdc2a16deeb6492bc5413fd8d1cbb2ff49d
nuralamsiddikey <nur.alam@lyxa.ai>
2025-12-16 23:55:17 +0600

    added hash table solution

--- Hash-table/Check if an array is subset of another array/index.js
+++ Hash-table/Check if an array is subset of another array/index.js
@@ -8,3 +8,14 @@ function isSubset(a, b) {
   }
   return true;
 }
+
+// has table
+function isSubset(a, b) {
+  const hashSet = new Set(a);
+  for (let i = 0; i < b.length; i++) {
+    if (!hashSet.has(b[i])) {
+      return false;
+    }
+  }
+  return true;
+}
