07470e2e974346b35d41c43e8038537d71a71202
nuralamsiddikey <nur.alam@lyxa.ai>
2025-12-18 00:15:46 +0600

    first non repeating character in a given string

---
+++ Hash-table/first non repeating character of a given string/index.js
@@ -0,0 +1,18 @@
+// first non repeating character of a given string
+
+function firstNonRepeatingCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    let found = false
+    for (let j = 0; j < str.length; j++) {
+      if (i !== j && str[i] === str[j]) {
+        found = true
+        break
+      } 
+    }
+    if(!found) return str[i]
+  }
+  return '$'
+}
+
+let str = "geeksforgeeks";
+console.log(firstNonRepeatingCharacter(str));
